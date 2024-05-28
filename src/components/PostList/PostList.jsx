4673cafa8f8f550764c9d6ae654bf158ab73b64b
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-05-28 13:13:44 +0300

    Post Item Ve Post List Componenti Oluşturuldu

---
+++ src/components/PostList/PostList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./PostList.module.css"
+const PostList = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default PostList
