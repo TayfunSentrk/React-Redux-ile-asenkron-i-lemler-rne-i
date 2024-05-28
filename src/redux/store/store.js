d6e5bdbc3ce5432fd2474ab65c3a9704aa057094
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-05-28 12:45:07 +0300

    Store için middleWare Yapısı kuruldu.CompositewithDevTools Eklentisi Eklendi

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,3 +3,11 @@ import { applyMiddleware, createStore } from "redux";
 
 import { thunk } from "redux-thunk";
 import postReducer from "../reducers/PostReducer";
+
+const middleWare=[thunk];
+
+const middleWareSample=applyMiddleware([...middleWare]);
+
+const store=createStore(postReducer,composeWithDevTools(middleWareSample));
+
+export default store
